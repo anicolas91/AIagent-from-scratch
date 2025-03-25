f95daaa89c51a7a5cfd4d67e08ccaf9a5b7d13b5
anicolas91 <andreanicolas91@gmail.com>
2025-03-25 13:41:19 -0700

    feat(agent.ts): added a while loop so the agent can retry tools and such until it gets an answer

--- src/agent.ts
+++ src/agent.ts
@@ -24,38 +24,46 @@ export const runAgent = async ({
   // put a message to show the agent is working on it
   const loader = showLoader('Let me think about it...')
 
-  // retrieve the entire convo history
-  const history = await getMessages()
+  // loop the system until the LLM gives back a string response
+  while (true) {
+    
+    // retrieve the entire convo history
+    const history = await getMessages()
 
-  // run the LLM, passing all the msg history and the tool
-  const response = await runLLM({
-    messages: history,
-    tools,
-  })
+    // run the LLM, passing all the msg history and the tool
+    const response = await runLLM({
+      messages: history,
+      tools,
+    })
 
-  // add the resulting msg to the history and log it
-  await addMessages([response])
-  logMessage(response)
+    // add the resulting msg to the history and log it
+    await addMessages([response])
+    logMessage(response)
 
-  // check if string or fcn came back
-  if (response.tool_calls){
-    // get the response, whether its a fcn or a string
-    const toolCall = response.tool_calls[0] // if many this gets for looped
-    loader.update(`executing: ${toolCall.function.name}`)
+    // if response is a string, then log the info
+    if (response.content){
+      // stop the loading animation
+      loader.stop()
 
-    // actually run the tool
-    const toolResponse = await runTool(toolCall,userMessage)
+      // give back the response
+      return getMessages()
+    }
 
-    // save the resulting msg to the history
-    await saveToolResponse(toolCall.id, toolResponse)
-    loader.update(`executed: ${toolCall.function.name}`)
+    // if a tool came back, then run it
+    if (response.tool_calls){
+      // get the response, whether its a fcn or a string
+      const toolCall = response.tool_calls[0] // if many this gets for looped
+      loader.update(`executing: ${toolCall.function.name}`)
 
-  }
+      // actually run the tool
+      const toolResponse = await runTool(toolCall,userMessage)
 
+      // save the resulting msg to the history
+      await saveToolResponse(toolCall.id, toolResponse)
+      loader.update(`executed: ${toolCall.function.name}`)
+
+    }
+
+  }  
   
-  // stop the loading animation
-  loader.stop()
-
-  // give back the response
-  return getMessages()
 }
